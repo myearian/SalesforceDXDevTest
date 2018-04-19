fb2d2fd77e7fcfa38c5c1b9bec46ee712b944af5
Matthew Yearian <matt.yearian@physiciansmutual.com>
2018-04-19 10:56:47 -0500

    Add google maps api ajax call.

--- force-app/main/default/aura/LightTestMapComp/LightTestMapCompController.js
+++ force-app/main/default/aura/LightTestMapComp/LightTestMapCompController.js
@@ -1,6 +1,6 @@
 ({
     jsLoaded: function (component, event, helper) {
-        var map = L.map('map', {zoomControl: false}).setView([37.784173, -122.401557], 14);
+        var map = L.map('map', {zoomControl: false}).setView([41.31805658029149, -96.11388146970849], 14);
         L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
             {
                 attribution: 'Tiles © Esri'
